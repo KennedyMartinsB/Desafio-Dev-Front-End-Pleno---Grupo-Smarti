68029f6dadd3a96b95fe5a0d07a9a43fc96d9247
KennedyMartinsB <kenedy.martins185@gmail.com>
2025-10-16 01:20:25 -0300

    Feature: criação de projeto, importação de frameworks, ferramentas e criação da store de produtos

---
+++ teste-grupo-smarti/src/stores/produtos.js
@@ -0,0 +1,27 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useProductStore = defineStore('product', () => {
+  const products = ref([])
+  const isLoading = ref(false)
+  const error = ref(null)
+
+  async function fetchProducts() {
+    isLoading.value = true
+    error.value = null
+    try {
+      const response = await fetch('https://fakestoreapi.com/products')
+      if (!response.ok) {
+        throw new Error('Falha ao buscar produtos.')
+      }
+      products.value = await response.json()
+    } catch (err) {
+      error.value = err.message
+      console.error(err)
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  return { products, isLoading, error, fetchProducts }
+})
